062f40602456e84e70ce41db40a5aaacd37f3bdb
Amit Kumar <amitnarayansah@gmail.com>
2021-03-22 11:18:18 +0530

    woking router

--- eStore/ClientUI/src/config.js
+++ eStore/ClientUI/src/config.js
@@ -1,5 +1,5 @@
-const CLIENT_ID = process.env.CLIENT_ID || '{clientId}';
-const ISSUER = process.env.ISSUER || 'https://{yourOktaDomain}.com/oauth2/default';
+const CLIENT_ID = process.env.CLIENT_ID || '0oacfqtylAFNYrEex5d6';
+const ISSUER = process.env.ISSUER || 'https://dev-63318095.okta.com/oauth2/defaultoauth2/default';
 const OKTA_TESTING_DISABLEHTTPSCHECK = process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false;
 
 export default {
