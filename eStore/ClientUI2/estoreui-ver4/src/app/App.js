5765ddfe82296dc147d0ffe9295690cdf5de2926
Amit Kumar <amitnarayansah@gmail.com>
2021-04-30 09:59:39 +0530

    version 4 UI Update

---
+++ eStore/ClientUI2/estoreui-ver4/src/app/App.js
@@ -0,0 +1,16 @@
+//import logo from '../logo.svg';
+import '../App.css';
+
+function App() {
+  return (
+    <div className="">
+      <header className="App-header">
+        <div>
+          WelCome to Aprajita Retails
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
